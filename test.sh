fa91724fd33c4aa0a016972f954c4e472d380884
Srikanth1916 <39981109+Srikanth1916@users.noreply.github.com>
2021-11-29 13:09:05 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
-	echo "Adding the dynatrace url and enabling the istio to the custom resource yaml file"
-  wget $CUSTOM_RESOURCE  && sed -i 's/# enableIstio: false/ enableIstio: true/g;s/ENVIRONMENTID/cwj81565/g' cr.yaml
+  echo "Adding the dynatrace url and enabling the istio to the custom resource yaml file"
+  wget $CUSTOM_RESOURCE  && sed -i 's/# enableIstio: false/enableIstio: true/g;s/ENVIRONMENTID/cwj81565/g' cr.yaml
   sleep 5
 	kubectl apply -f cr.yaml
