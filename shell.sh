0185aa743fb087e7bd3d27b1e7962e6edbaadd3e
unknown <srikanth@skyhive.io>
2022-01-05 13:29:19 +0530

    dd

--- shell.sh
+++ shell.sh
@@ -1,16 +1,18 @@
+
+echo "fetch content"
 git fetch --unshallow --tags		
-git tag -l > file
-cat file
+echo "tagging content"
+git tag -l 
 echo "first"
 echo github.ref
 echo "second"
 echo ${{ github.ref }}
-/*
-if grep ${{ github.ref }} file
-then
-   echo "Match found and good to proceed with the next steps"
-else
-   echo "Match does not found"
-exit 1 
-fi
-*/
+
+#if grep ${{ github.ref }} file
+#then
+#   echo "Match found and good to proceed with the next steps"
+#else
+#   echo "Match does not found"
+#exit 1 
+#fi
+
