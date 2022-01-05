4d95941444d711ca1ab7b8a0388f1732f4ab559e
unknown <srikanth@skyhive.io>
2022-01-05 12:30:07 +0530

    commited

--- shell.sh
+++ shell.sh
@@ -1,6 +1,11 @@
 git fetch --unshallow --tags		
 git tag -l > file
 cat file
+echo "first"
+echo github.ref
+echo "second"
+echo ${{ github.ref }}
+/*
 if grep ${{ github.ref }} file
 then
    echo "Match found and good to proceed with the next steps"
@@ -8,3 +13,4 @@ else
    echo "Match does not found"
 exit 1 
 fi
+*/
