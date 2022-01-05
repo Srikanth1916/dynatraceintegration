ad63d8b70759d1d229fbbf305c0b98026d8efc4d
unknown <srikanth@skyhive.io>
2022-01-05 14:58:05 +0530

    updated shell.sh

--- shell.sh
+++ shell.sh
@@ -1,18 +1,12 @@
+git fetch --unshallow --tags
+git tag -l > file1
+echo ${{ github.ref }} | awk -F '/' '{print $3}' > file2
+C=`cat file2`
 
-echo "fetch content"
-git fetch --unshallow --tags		
-echo "tagging content"
-git tag -l 
-echo "first"
-echo github.ref
-echo "second"
-echo ${{ github.ref }}
-
-#if grep ${{ github.ref }} file
-#then
-#   echo "Match found and good to proceed with the next steps"
-#else
-#   echo "Match does not found"
-#exit 1 
-#fi
-
+if grep $C file1
+then
+  echo "Latest tag is found and good to proceed with the next steps"
+else
+  echo "Latest tag is not found"
+exit 1
+fi
